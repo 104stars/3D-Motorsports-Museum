67980683065495996cf07e932da9cf3b4cde5481
AllOfTheLightsInterlude <alejandro.lasso@correounivalle.edu.co>
2026-05-21 21:52:17 -0500

    hieght test 2

--- components/PlayerController.jsx
+++ components/PlayerController.jsx
@@ -131,7 +131,7 @@ export default function PlayerController({ spawn = DEFAULT_SPAWN }) {
       // --- 5. MOVEMENT ---
       maxVelLimit={6}
       sprintMult={1.8}
-      jumpVel={6.4}
+      jumpVel={7}
       jumpForceToGroundMult={0}
       sprintJumpMult={1}
       fallingGravityScale={2.7}
